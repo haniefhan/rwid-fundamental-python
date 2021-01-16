e850324919e7fccaca7833ffcfb6ba3e0d2ff97f
Hanief <haniefhan@gmail.com>
2021-01-17 06:39:10 +0700

    scraping course :: rates + templating using jinjaa :: scrap rates data from http://www.floatrates.com/daily/idr.json

--- run.py
+++ run.py
@@ -22,7 +22,17 @@ def detik_populer():
     titles = content.findAll('h3', attrs={'class': 'media__title'})
     images = content.findAll('div', attrs={'class': 'media__image'})
 
-    return render_template('index.html', images=images)
+    return render_template('detik_populer.html', images=images)
+
+
+@app.route('/idr-rates')
+def idr_rates():
+    url = "http://www.floatrates.com/daily/idr.json"
+    json = requests.get(url)
+
+    rates = json.json().values()
+
+    return render_template('idr_rates.html', rates=rates)
 
 
 if __name__ == "__main__":
